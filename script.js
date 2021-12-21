b29e6f795f32ab769896b31bd940d99acfa42bf4
Paulo Porto <prtpj1@gmail.com>
2021-12-21 18:00:47 -0300

    Req 7 - Done

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-function newTask() {
-    let inputField = document.querySelector('#texto-tarefa');
-    let inputBtn = document.querySelector('#criar-tarefa');
-    let taskList = document.querySelector('#lista-tarefas');
+window.onload = function () {
+  let inputField = document.querySelector('#texto-tarefa');
+  let inputBtn = document.querySelector('#criar-tarefa');
+  let taskList = document.querySelector('#lista-tarefas');
 
+  function newTask() {
     inputBtn.addEventListener('click', () => {
-        if (inputField.value.length > 0) {
-            let newLi = document.createElement('li');
-            newLi.innerText = inputField.value;
+      if (inputField.value.length > 0) {
+        let newLi = document.createElement('li');
+        newLi.innerText = inputField.value;
+        newLi.className = 'task';
 
-            taskList.appendChild(newLi);
-            inputField.value = '';
-        } else {
-            alert('Você não digitou nenhuma tarefa!');
-        }
+        taskList.appendChild(newLi);
+        inputField.value = '';
+      } else {
+        alert('Você não digitou nenhuma tarefa!');
+      }
     });
 
     inputField.addEventListener('keyup', (event) => {
-        if (event.key === 'Enter' && inputField.value.length > 0) {
-            let newLi = document.createElement('li');
-            newLi.innerText = inputField.value;
+      if (event.key === 'Enter' && inputField.value.length > 0) {
+        let newLi = document.createElement('li');
+        newLi.innerText = inputField.value;
+        newLi.className = 'task';
 
-            taskList.appendChild(newLi);
-            inputField.value = '';
-        }
+        taskList.appendChild(newLi);
+        inputField.value = '';
+      }
     });
-};
+  }
 
-newTask();
+  newTask();
+  function taskSelect() {
+    taskList.addEventListener('click', function (event) {
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
+  taskSelect();
+};
